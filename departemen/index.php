ffd3c4fedfc4b0495689e8db8cb268217167c8b8
joshuayordana <joshuayordana8@gmail.com>
2022-11-18 14:26:58 +0700

    Departemen

--- departemen/index.php
+++ departemen/index.php
@@ -18,18 +18,18 @@
 	<div id="contentContainer" class="trans3d"> 
 	<section id="carouselContainer" class="trans3d">
 		
-		<figure id="item1" class="carouselItem trans3d"><a href="http://google.com"><div class="trans3d"><img src="maro.jpg" alt="maro.jpg" style="max-width: 200px;"></div></a></figure>
+		<figure id="item1" class="carouselItem trans3d"><a href="http://google.com"><div class="trans3d"><img src="./asset/ir.png" alt="maro.jpg" style="max-width: 300px;"></div></a></figure>
 		<figure id="item2" class="carouselItem trans3d"><div class="carouselItemInner trans3d">2</div></figure>
 		<figure id="item3" class="carouselItem trans3d"><div class="carouselItemInner trans3d">3</div></figure>
 		<figure id="item4" class="carouselItem trans3d"><div class="carouselItemInner trans3d">4</div></figure>
-		<figure id="item5" class="carouselItem trans3d"><div class="carouselItemInner trans3d">5</div></figure>
+		<!-- <figure id="item5" class="carouselItem trans3d"><div class="carouselItemInner trans3d">5</div></figure>
 		<figure id="item6" class="carouselItem trans3d"><div class="carouselItemInner trans3d">6</div></figure>
 		<figure id="item7" class="carouselItem trans3d"><div class="carouselItemInner trans3d">7</div></figure>
 		<figure id="item8" class="carouselItem trans3d"><div class="carouselItemInner trans3d">8</div></figure>
 		<figure id="item9" class="carouselItem trans3d"><div class="carouselItemInner trans3d">9</div></figure>
 		<figure id="item10" class="carouselItem trans3d"><div class="carouselItemInner trans3d">10</div></figure>
 		<figure id="item11" class="carouselItem trans3d"><div class="carouselItemInner trans3d">11</div></figure>
-		<figure id="item12" class="carouselItem trans3d"><div class="carouselItemInner trans3d">12</div></figure>	
+		<figure id="item12" class="carouselItem trans3d"><div class="carouselItemInner trans3d">12</div></figure>	 -->
 	</section>
 	</div>
 <!-- partial -->
